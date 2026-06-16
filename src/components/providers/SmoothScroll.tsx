f8b532003d2ec72cd43c0513c43f2963fc0b8e65
Subham Karmakar <rikk4335@gmail.com>
2026-06-16 08:08:12 +0530

    fixed

--- src/components/providers/SmoothScroll.tsx
+++ src/components/providers/SmoothScroll.tsx
@@ -1,14 +1,9 @@
 'use client'
 
-import { useEffect, useState, createContext, useContext } from 'react'
-import Lenis from 'lenis'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-
-gsap.registerPlugin(ScrollTrigger)
+import { createContext, useContext } from 'react'
 
 interface LenisContextValue {
-  lenis: Lenis | null
+  lenis: null
 }
 
 const LenisContext = createContext<LenisContextValue>({ lenis: null })
@@ -18,35 +13,8 @@ export function useLenis() {
 }
 
 export function SmoothScroll({ children }: { children: React.ReactNode }) {
-  const [lenisInstance, setLenisInstance] = useState<Lenis | null>(null)
-
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      smoothWheel: true,
-      syncTouch: true,
-    })
-
-    setLenisInstance(lenis)
-
-    lenis.on('scroll', ScrollTrigger.update)
-
-    const update = (time: number) => {
-      lenis.raf(time * 1000)
-    }
-
-    gsap.ticker.add(update)
-    gsap.ticker.lagSmoothing(0)
-
-    return () => {
-      gsap.ticker.remove(update)
-      lenis.destroy()
-      setLenisInstance(null)
-    }
-  }, [])
-
   return (
-    <LenisContext.Provider value={{ lenis: lenisInstance }}>
+    <LenisContext.Provider value={{ lenis: null }}>
       {children}
     </LenisContext.Provider>
   )
