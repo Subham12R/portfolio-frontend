7f6a3fa278671c27d9c48c83e3d8aaa214baef64
Subham Karmakar <rikk4335@gmail.com>
2026-09-16 01:36:58 +0530

    updated marginal issues

--- src/sections/hero/page.tsx
+++ src/sections/hero/page.tsx
@@ -74,7 +74,7 @@ export const Hero = () => {
           </div>
           <div className="flex flex-col items-start justify-center gap-0.5 min-w-0 w-full">
             {/* Name */}
-            <h1 className="text-3xl font-light tracking-tight text-text-primary font-instrumentserif truncate w-full">
+            <h1 className="text-3xl font-light tracking-tight text-text-primary font-instrumentserif truncate w-full mt-10">
               {name}
             </h1>
             <p className="text-[14px] text-text-secondary/70 font-light font-instrumentsans tracking-tight">
